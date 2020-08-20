2392a1b6eeb7bdf75f49613c1c3c06fefc407f80
TheGreatRambler <aehart684351@gmail.com>
2020-08-20 18:01:05 -0400

    Initial work

---
+++ BlankPlugin/src/main/java/dev/cibmc/spigot/blankplugin/App.java
@@ -0,0 +1,13 @@
+package dev.cibmc.spigot.blankplugin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Some_Mods
 Some Minecraft mods, may eventually make some
+TODO use this https://www.spigotmc.org/wiki/creating-a-blank-spigot-plugin-in-vs-code/
