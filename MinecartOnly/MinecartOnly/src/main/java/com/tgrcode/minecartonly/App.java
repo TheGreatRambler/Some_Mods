c1ddcd0177f2fdf9d50a71c49a8f444b0c134755
TheGreatRambler <aehart684351@gmail.com>
2020-08-24 17:17:53 -0400

    Change minecart plugin goal

--- MinecartOnly/MinecartOnly/src/main/java/com/tgrcode/minecartonly/App.java
+++ MinecartOnly/MinecartOnly/src/main/java/com/tgrcode/minecartonly/App.java
@@ -2,15 +2,23 @@ package com.tgrcode.minecartonly;
 
 import java.util.logging.Logger;
 import org.bukkit.Material;
+import org.bukkit.Server.Spigot;
+import org.bukkit.block.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.inventory.*;
+import org.bukkit.material.*;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.*;
+import org.bukkit.util.io.BukkitObjectInputStream;
 
 public class App extends JavaPlugin implements Listener {
+	private Block lastRailBlock     = null;
+	private Block lastLastRailBlock = null;
+
 	@Override
 	public void onEnable() {
 		getLogger().info("Start MinecartOnly");
@@ -23,19 +31,26 @@ public class App extends JavaPlugin implements Listener {
 
 	@EventHandler
 	public void onPlayerJoin(PlayerJoinEvent event) {
-		setPlayersHand(event.getPlayer().getInventory());
+		// setPlayersHand(event.getPlayer().getInventory());
+
+		org.bukkit.inventory.ItemStack minecarts = new org.bukkit.inventory.ItemStack(Material.MINECART, 64);
+		event.getPlayer().getInventory().setItem(8, minecarts);
+	}
+
+	@EventHandler
+	public void onPlayerMove(PlayerMoveEvent event) {
+		Block thisBlock = event.getTo().getBlock();
+
+		if(lastRailBlock != null && lastLastRailBlock != null) {
+			// TODO, automatically place rails underneath
+		}
+
+		lastLastRailBlock = lastRailBlock;
+		lastRailBlock     = thisBlock;
 	}
 
 	@EventHandler
 	public void onBlockPlace(BlockPlaceEvent event) {
-		setPlayersHand(event.getPlayer().getInventory());
-	}
-
-	private void setPlayersHand(PlayerInventory inventory) {
-		getLogger().info("Fix inventory");
-		inventory.setItem​(9, new ItemStack​(Material.MINECART, 64));
-		inventory.setItem​(8, new ItemStack​(Material.POWERED_RAIL, 64));
-		inventory.setItem​(7, new ItemStack​(Material.RAILS));
-		inventory.setItem​(6, new ItemStack​(Material.REDSTONE_TORCH_ON, 64));
+		//
 	}
 }
